76073fd0bc1fb2d5ddbcd73733cde89467f98438
Jonathan <jonathan.chanci@hotmail.com>
2020-03-01 16:14:29 -0500

    Fin del proyecto

--- ConsumoFrontPaises/Departamento/index.aspx
+++ ConsumoFrontPaises/Departamento/index.aspx
@@ -1,5 +1,62 @@
 ﻿<%@ Page MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ConsumoFrontPaises.Departamento.index" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-            <h1>Prueba de index departamentos</h1>
+    <h1>Lista de Departamentos</h1>
+    <hr />
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formDep" id="btnNuevo">
+        <span class="glyphicon glyphicon-plus"></span>
+        Nuevo
+    </button>
+    <div class="row">
+        <table class="table" id="tblDep">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Id Pais</th>
+                    <th scope="col">Pais</th>
+                    <th scope="col">Gestión</th>
+                </tr>
+            </thead>
+            <tbody id="contentTable">
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="formDep" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Gestión de Departamentos</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <input type="hidden" id="idDep" />
+                        <div class="form-group">
+                            <label for="nombreDep">Nombre</label>
+                            <input type="text" class="form-control" id="nombreDep" placeholder="Ingrese el nombre del pais">
+                        </div>
+                        <div class="form-group">
+                            <label for="descDep">Descripción</label>
+                            <textarea rows="3" class="form-control" id="descDep" placeholder="Ingrese la descripcion del pais"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="idPais">Pais</label>
+                            <select class="form-control" id="idPais">
+                                <option value="">-- Seleecione un pais --</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="button" id="btnSubmit" class="btn btn-primary">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../Scripts/Departamento/gestion.js"></script>
 </asp:Content>
--- ConsumoFrontPaises/Scripts/Departamento/gestion.js
+++ ConsumoFrontPaises/Scripts/Departamento/gestion.js
@@ -1,1 +1,178 @@
-﻿
+﻿$(function () {
+
+    $("#btnNuevo").click(function () {
+        $("#idDep").val("");
+    });
+
+    $.ajax({
+        url: "http://localhost:58525/api/pais/",
+        type: "get",
+        success: function (res) {
+            var paises = "";
+            $.each(res, function (i, v) {
+                paises += `<option value='${v.IdPais}'> ${v.NombrePais}</option>`;
+            });
+            $("#idPais").append(paises);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+
+    $.ajax({
+        url: "http://localhost:58525/api/Departamentos",
+        type: "get",
+        success: function (res) {
+            var table = "";
+            $.each(res, function (i, v) {
+                table += `
+                            <tr>
+                                <th scope="row">${v.id}</th>
+                                <td>${v.nombre}</td>
+                                <td>${v.descripcion}</td>
+                                <td>${v.idPais}</td>
+                                <td>${getNamePais(v.idPais).NombrePais}</td>
+                                <td>
+                                    <button class="btn btn-warning" title="Editar" type="button">
+                                        <span class="glyphicon glyphicon-pencil"></span>
+                                    </button> |
+                                    <button class="btn btn-danger" title="Eliminar" type="button">
+                                        <span class="glyphicon glyphicon-trash"></span>
+                                    </button> 
+                                </td>
+                            </tr>
+                        `;
+            });
+            $("#contentTable").append(table);
+        },
+        error: function (err) {
+            console.log(err);
+        },
+        complete: function (jqXHR, textStatus) {
+            if (textStatus === "success") {
+                //Eliminar
+                $(".btn-danger").click(function () {
+                    var fila = $(this).closest("tr");
+                    var idDep = fila[0].children[0].textContent;
+                    var nombreDep = fila[0].children[1].textContent;
+                    Swal.fire({
+                        title: `Estas seguro ?`,
+                        text: `Eliminaras definitivamente a ${nombreDep}!`,
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Si, eliminar!'
+                    }).then((result) => {
+                        if (result.value) {
+                            $.ajax({
+                                url: "http://localhost:58525/api/departamentos/" + idDep,
+                                type: "DELETE",
+                                success: function (res) {
+                                    Swal.fire(
+                                        'Eliminado!',
+                                        `Ya no existe ${nombreDep}`,
+                                        'success'
+                                    ).then(() => location.reload());
+                                },
+                                error: function (err) {
+                                    console.log(err);
+                                }
+                            });
+
+                        }
+                    });
+
+                });
+                //Actualizar
+                $(".btn-warning").click(function () {
+                    var fila = $(this).closest("tr");
+                    var idDep = fila[0].children[0].textContent;
+                    var nombreDep = fila[0].children[1].textContent;
+                    var descDep = fila[0].children[2].textContent;
+                    var idPais = fila[0].children[3].textContent;
+
+                    $("#nombreDep").val(nombreDep);
+                    $("#descDep").val(descDep);
+                    $("#idDep").val(idDep);
+                    $("#idPais").val(idPais);
+                    $("#formDep").modal("show");
+
+                });
+            }
+        }
+    });
+
+    function getNamePais(idPais) {
+        var pais = "";
+        $.ajax({
+            url: "http://localhost:58525/api/pais/" + idPais,
+            type: "get",
+            async : false,
+            success: function (res) {
+                pais = res;           
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+        return pais;
+    }
+
+    $("#btnSubmit").click(function () {
+        var nombreDep = $("#nombreDep").val();
+        var descDep = $("#descDep").val();
+        var idDep = $("#idDep").val();
+        var idPais = $("#idPais").val();
+        var tipo = "POST";
+
+        if (idDep !== "") {
+            tipo = "PUT";
+        }
+
+        var validationForm = formIsValid(nombreDep,idPais);
+        if (validationForm === "") {
+            var Dep = new Object();
+            if (idDep !== "") {
+                Dep.id = idDep;
+            }
+            Dep.nombre = nombreDep;
+            Dep.descripcion = descDep;
+            Dep.idPais = idPais;
+
+            $.ajax({
+                url: "http://localhost:58525/api/departamentos/" + idDep,
+                type: tipo,
+                data: Dep,
+                success: function (res) {
+                    console.log(res);
+                    cerrarModal();
+                    location.reload();
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        } else {
+            alert(validationForm);
+        }
+    });
+
+    function formIsValid(nombre, pais) {
+        var msg = "";
+        if (nombre === "" || nombre === null || nombre === undefined) {
+            msg = "El campo nombre es requerido \n";
+        }
+        if (pais === "" || pais === null || pais === undefined) {
+            msg += "El campo pais es requerido \n";
+        }
+        return msg;
+    }
+
+    function cerrarModal() {
+        $("#nombreDep").val("");
+        $("#descDep").val("");
+        $("#idPais").val("");
+        $("#formDep").modal("hide");
+    }
+});
